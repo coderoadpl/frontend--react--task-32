182b3ff8952bb6821bd5552d88207c034bd183b4
Mateusz Choma <kontakt@coderoad.pl>
2021-06-22 18:03:46 +0200

    Reset all button added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
 import Counter from './Counter'
+import Button from './Button'
 
 export const App = () => {
   return (
     <div>
+      <Button
+        label={'RESET ALL'}
+        onClick={console.log}
+      />
       <Counter />
       <Counter
         initialNumber={5}
